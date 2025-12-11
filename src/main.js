49dfd917ecd0893701dda0a84757fbf9f68fe7b4
Timo Triisa <timotriisa@gmail.com>
2025-12-11 12:22:58 +0200

    fix: correct API_URL constant to include '/api' path

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { navigateTo, router } from "./router.js";
 
-export const API_URL = "http://192.168.114.50";
+export const API_URL = "http://192.168.114.50/api";
 
 document.addEventListener("click", (e) => {
   if (e.target.matches("[data-link]")) {
